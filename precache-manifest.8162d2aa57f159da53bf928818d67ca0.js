1c1a93fe278efa51729da9fd31d5252952323aae
Mo7arb-art <mos7997@gmail.com>
2025-08-31 20:54:37 +0300

    Updates

---
+++ precache-manifest.8162d2aa57f159da53bf928818d67ca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64243ba84bffab0942c22871296b5d8",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "88dc1e89de34256894d4",
+    "url": "/Todo-List/static/css/main.d1a43f97.chunk.css"
+  },
+  {
+    "revision": "7debcf365ef9b8f274fc",
+    "url": "/Todo-List/static/js/2.c4ac2bc1.chunk.js"
+  },
+  {
+    "revision": "88dc1e89de34256894d4",
+    "url": "/Todo-List/static/js/main.c23436b2.chunk.js"
+  },
+  {
+    "revision": "7cc9c631789ec872fc79",
+    "url": "/Todo-List/static/js/runtime-main.247c93a0.js"
+  }
+]);
